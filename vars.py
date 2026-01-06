b682fb161e307bcb3e33dece6da6eb417d3a67d5
CHENARAMKHICHAR2003 <crchoudharyji1122@gmail.com>
2026-01-06 14:26:29 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -7,8 +7,8 @@ API_HASH = environ.get("API_HASH", "b88da5f4f338cca30f8ea5fb53cb083b")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 # Force Subscribe Configuration
-FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "")  # Channel username without @, 
-FORCE_SUB_CHANNEL_LINK = environ.get("FORCE_SUB_CHANNEL_LINK","")  # Channel link
+FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "force_subscribe")  # Channel username without @, 
+FORCE_SUB_CHANNEL_LINK = environ.get("FORCE_SUB_CHANNEL_LINK","https://t.me/force_subscribe")  # Channel link
 
 # Admin Configuration
 ADMINS = list(map(int, environ.get("ADMINS", "7693352537").split()))
@@ -18,6 +18,7 @@ OWNER_ID = int(environ.get("OWNER_ID", "7693352537"))
 
 # Database URL (if you want to add database support later)
 DATABASE_URL = environ.get("DATABASE_URL", "mongodb+srv://divyanshshukla5375_db_user:1kZ2dsVTktdMljpr@cluster0.lo5qk5v.mongodb.net/?appName=Cluster0")
+
 
 
 
